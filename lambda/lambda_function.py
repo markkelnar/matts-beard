a30b8311b15346862cc73983bb4a6f96cade9d06
Mark Kelnar <markkelnar@gmail.com>
2020-04-16 12:55:31 -0500

    Keep prompt open

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -49,8 +49,7 @@ class HelloWorldIntentHandler(AbstractRequestHandler):
 
         return (
             handler_input.response_builder
-                .speak(speak_output)
-                # .ask("add a reprompt if you want to keep the session open for the user to respond")
+                .ask(speak_output)
                 .response
         )
 
