e5f1529dc0e76abe2267329b5b760345d41ecc9e
Mark Kelnar <markkelnar@gmail.com>
2020-04-17 20:54:35 -0500

    Remove hello world handler

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -158,7 +158,6 @@ class CatchAllExceptionHandler(AbstractExceptionHandler):
 sb = SkillBuilder()
 
 sb.add_request_handler(LaunchRequestHandler())
-sb.add_request_handler(HelloWorldIntentHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
